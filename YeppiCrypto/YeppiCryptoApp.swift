22e9b8369ce7d39fc74aaaa5d41e00ff2e3bbc06
js-min <alswltn1@korea.ac.kr>
2022-06-08 21:41:18 +0900

    Initial Commit

---
+++ YeppiCrypto/YeppiCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  YeppiCryptoApp.swift
+//  YeppiCrypto
+//
+//  Created by JISU MIN on 2022/06/08.
+//
+
+import SwiftUI
+
+@main
+struct YeppiCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
