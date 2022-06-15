710b50f26c79f4c2c9e74b008cc02d2adfcdd937
js-min <alswltn1@korea.ac.kr>
2022-06-15 22:12:01 +0900

    #9 - Saving images to the File Manager

--- YeppiCrypto/Services/CoinImageService.swift
+++ YeppiCrypto/Services/CoinImageService.swift
@@ -14,15 +14,28 @@ class CoinImageService {
   @Published var image: UIImage? = nil
   
   private var imageSubscription: AnyCancellable?
-  
   private let coin: Coin
+  private let fileManager = LocalFileManager.instance
+  private let folderName = "coin_images"
+  private let imageName: String
   
   init(coin: Coin) {
     self.coin = coin
+    self.imageName = coin.id
     getCoinImage()
   }
   
   private func getCoinImage() {
+    if let savedImage = fileManager.getImage(imageName: imageName, folderName: folderName) {
+      print("DEBUG: retrieved image from File Manager")
+      image = savedImage
+    } else {
+      downloadCoinImage()
+    }
+  }
+  
+  private func downloadCoinImage() {
+    print("DEBUG: download image now")
     guard let url = URL(string: coin.image) else { return }
     
     imageSubscription = NetworkManager.download(url: url)
@@ -30,8 +43,13 @@ class CoinImageService {
         return UIImage(data: data)
       })
       .sink(receiveCompletion: NetworkManager.handleCompletion, receiveValue: { [weak self] returnedImage in
-        self?.image = returnedImage
-        self?.imageSubscription?.cancel()
+        guard
+          let self = self,
+          let downloadedImage = returnedImage
+        else { return }
+        self.image = downloadedImage
+        self.imageSubscription?.cancel()
+        self.fileManager.saveImage(image: downloadedImage, imageName: self.imageName, folderName: self.folderName)
       })
   }
   
